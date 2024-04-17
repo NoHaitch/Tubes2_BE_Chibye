615c9e4105978dd80b67d7146bea4c373e2381f3
Chiko but no H <franciscotrianto@gmail.com>
2024-04-17 17:04:58 +0700

    feat: IDS search for one path

--- src/go.mod
+++ src/go.mod
@@ -2,8 +2,9 @@ module example/src
 
 go 1.22.1
 
+require github.com/PuerkitoBio/goquery v1.9.1
+
 require (
-	github.com/PuerkitoBio/goquery v1.9.1 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	golang.org/x/net v0.21.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -5,21 +5,25 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	pageTitle := "Joko_Widodo"
-	wikiLinks, err := scrapeWikipediaLinks(pageTitle)
-	if err != nil {
-		log.Fatal(err)
+	pageTitleStart := "Joko_Anwar"
+	pageTitleEnd := "Kampala"
+
+	startTime := time.Now()
+
+	if idsStart(pageTitleStart, pageTitleEnd, 3) {
+		fmt.Println("FOUND")
+	} else {
+		fmt.Println("NOT FOUND")
 	}
 
-	fmt.Println("Wikipedia Links:")
-	for _, link := range wikiLinks {
-		fmt.Println(link)
-	}
+	elapsedTime := time.Since(startTime)
+	fmt.Printf("Execution Time: %f seconds\n", elapsedTime.Seconds())
 }
 
 func scrapeWikipediaLinks(pageTitle string) ([]string, error) {
@@ -45,7 +49,7 @@ func scrapeWikipediaLinks(pageTitle string) ([]string, error) {
 	}
 
 	// List of links
-	var wikiLinks []string
+	var wikiTitles []string
 
 	// selects all <a> element
 	doc.Find("#mw-content-text a").Each(func(i int, s *goquery.Selection) {
@@ -59,10 +63,69 @@ func scrapeWikipediaLinks(pageTitle string) ([]string, error) {
 				!strings.HasPrefix(href, "/wiki/Template:") && !strings.HasPrefix(href, "/wiki/Help:") &&
 				!strings.HasPrefix(href, "/wiki/Special:") && !strings.HasPrefix(href, "/wiki/Template_talk:") &&
 				!strings.HasPrefix(href, "/wiki/Category:") && !strings.HasPrefix(href, "/wiki/Wikipedia:") {
-				wikiLinks = append(wikiLinks, "https://en.wikipedia.org"+href)
+
+				// Remove prefix
+				pageTitle := strings.TrimPrefix(href, "/wiki/")
+				wikiTitles = append(wikiTitles, pageTitle)
 			}
 		}
 	})
 
-	return wikiLinks, nil
+	return wikiTitles, nil
+}
+
+func ids(srcTitle string, targetTitle string, limit int, currPath []string) ([]string, bool) {
+	fmt.Printf("> %d %s\n", limit, srcTitle)
+	if srcTitle == targetTitle {
+		return currPath, true
+	}
+
+	if limit <= 0 {
+		return nil, false
+	}
+
+	wikiTitles, err := scrapeWikipediaLinks(srcTitle)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// search for target
+	for i := range wikiTitles {
+		seen := false
+		for _, link := range currPath {
+			if wikiTitles[i] == link {
+				seen = true
+				break
+			}
+		}
+
+		if seen {
+			continue
+		}
+
+		newPath := append(currPath, wikiTitles[i])
+		resultPath, found := ids(wikiTitles[i], targetTitle, limit-1, newPath)
+		if found {
+			return resultPath, true
+		}
+	}
+
+	fmt.Println(" == Finish a ids")
+
+	return nil, false
+}
+
+func idsStart(srcTitle string, targetTitle string, maxDepth int) bool {
+	for limit := 0; limit <= maxDepth; limit++ {
+		path, found := ids(srcTitle, targetTitle, limit, nil)
+		if found {
+			fmt.Println("Wikipedia Links:")
+			fmt.Printf(" 1. %s\n", srcTitle)
+			for i, title := range path {
+				fmt.Printf(" %d. %s \n", i+2, title)
+			}
+			return true
+		}
+	}
+	return false
 }
